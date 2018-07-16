23df167a15f5613bf7bdde95b2cb518b4f7ec984
Marc <mnraaz@gmail.com>
2018-07-16 10:53:38 +1000

    Added user friendly commands and fixed indentation

--- src/move_cli.py
+++ src/move_cli.py
@@ -8,16 +8,18 @@ try:
     while True:
         Ab.stop()
         user_input = raw_input("What should I do now?")
-        if user_input == "forward":
-                Ab.forward()
-        elif user_input == "backward":
-                Ab.backward()
-        elif user_input == "left":
-                Ab.left()
-        elif user_input == "right":
-                Ab.right()
+        if user_input == "forward" or user_input.lower() == "f":
+            Ab.forward()
+        elif user_input == "backward" or user_input.lower() == "b" :
+            Ab.backward()
+        elif user_input == "left" or user_input.lower() == "l":
+            Ab.left()
+        elif user_input == "right" or user_input.lower() == "r":
+            Ab.right()
+        elif user_input == "x":
+            break
         else:
-                print("That command isnt valid.")
+            print("That command isnt valid. Please try f=forward, b=backwards, l=left, r=right or x to exit.")
         time.sleep(1)
 except KeyboardInterrupt:
     GPIO.cleanup()
